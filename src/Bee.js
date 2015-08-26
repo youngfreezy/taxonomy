4000ea6d7fa88906cbe874260b31224968adfd3d
Fareez Ahmed <Fareez.Ahmed@gmail.com>
2015-08-26 12:06:24 -0700

    tests pass

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,13 @@
 var Bee = function() {
+  //overwriting inherited properties;
+  Grub.call(this, arguments)
+  this.age = 5;
+  this.color = "yellow";
+  this.job = "keep on growing";
+  
 };
 
+//bee inherits all the methods on the grub prototype. 
+
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,15 @@
 var HoneyMakerBee = function() {
+  Bee.call(this, arguments);
+  this.age = 10;
+  this.job = 'make honey';
+  this.honeyPot = 0;
 };
 
+HoneyMakerBee.prototype = Object.create(Grub.prototype);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+HoneyMakerBee.prototype.makeHoney = function(){
+  this.honeyPot++;
+}
+HoneyMakerBee.prototype.giveHoney = function(){
+  this.honeyPot--;
+}
